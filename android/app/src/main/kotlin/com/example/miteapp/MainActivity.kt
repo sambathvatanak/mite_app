04c2438064eb78d0de9be3f989bc466499c4c3c9
vatanaksambath <sambathvatanak@gmail.com>
2021-02-26 21:05:56 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/miteapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.miteapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/playstore.dart
@@ -0,0 +1,314 @@
+import 'package:flutter/material.dart';
+
+class playStore extends StatefulWidget {
+  @override
+  _playStoreState createState() => _playStoreState();
+}
+
+Widget _rowOne(){
+  return Stack(
+    alignment: Alignment.center,
+    children: <Widget>[
+      Container(
+          height: 240,
+          decoration: new BoxDecoration(
+              color: Colors.white,
+            image: DecorationImage(
+              image: AssetImage('assets/Preview.jpg'),
+              fit: BoxFit.fill,
+            ),
+          ),
+      ),
+      IconButton(
+            iconSize: 90.0,
+            icon: Icon(Icons.play_circle_fill),
+        ),
+    ],
+  );
+}
+
+Widget _rowTwo(){
+  return Container(
+      margin: EdgeInsets.only(left: 10, right: 10, top: 20.0, bottom: 20.0),
+      height: 170,
+    child: Row(
+      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget> [
+         ClipRRect(
+            borderRadius: BorderRadius.circular(10.0),
+            child: Image.asset('assets/Thumbnail.jpg'),
+          ),
+        Container(
+          margin: EdgeInsets.only(left: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Text(
+                'Spider-Man:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                'Homecoming',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              Text(
+                '2017',
+                style: TextStyle(
+                  //ontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+          ],),
+        )
+    ],)
+  );
+}
+
+Widget _rowThree(){
+  return Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          margin: EdgeInsets.only(left: 10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                Text(
+                  '4.4',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(
+                      Icons.star_rate,
+                      color: Colors.black,
+                      size: 20.0,
+                ),
+              ],),
+              SizedBox(height: 10.0,),
+              Text(
+                '42K reviews',
+                style: TextStyle(
+                  //fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 5.0,),
+        Container(
+          width: 2,
+          height: 30,
+          color: Colors.grey,
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset('assets/Tomato.jpg', height: 20,),
+                Text(
+                  '92%',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ],),),
+              SizedBox(height: 10,),
+              Text(
+                'Tomatometer',
+                style: TextStyle(
+                  //fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 5.0,),
+        Container(
+          width: 2,
+          height: 30,
+          color: Colors.grey,
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '4.4',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 10,),
+              Text(
+                '42k reviews',
+                style: TextStyle(
+                  //fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget _rowFour(){
+  return Container(
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(height: 15.0),
+      Container(
+        height: 50,
+        margin: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          border: Border.all(width: 2.0, color: Colors.grey),
+        ),
+        child: Center(
+          child: Text('Rent from KHR 12,174.04',
+          style: TextStyle(
+            color: Colors.pink,
+            fontWeight: FontWeight.bold,
+          ),),
+        )
+      ),
+        Container(
+            height: 50,
+            margin: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: Colors.pink,
+              borderRadius: BorderRadius.circular(8.0),
+              border: Border.all(width: 2.0, color: Colors.pink),
+            ),
+            child: Center(
+              child: Text('Buy from KHR 42,710.92',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),),
+            )
+        ),
+    ],),
+  );
+}
+
+Widget _rowFive(){
+  return Container(
+    margin: EdgeInsets.only(left: 10.0),
+    child: Column(
+      children: [
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Text('About this movie',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),),
+              ),
+              SizedBox(width: 221.0,),
+              Container(
+                child: IconButton(
+                    icon: Icon(
+                      Icons.arrow_forward,
+                      color: Colors.black,
+                    ),
+                    onPressed: null
+                ),
+              ),
+            ],),
+        ),
+        Container(
+          margin: EdgeInsets.all(8.0),
+          child: Text('Spider-Main [Tom Holland] begin to navigate '
+              'his new identity as the web-sibling '
+              'super hero under the '
+              'watchful eye of his mentor Tony Stark',
+          style: TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.w400,
+            fontSize: 18.0
+          ),),
+        ),
+      ],
+    ),
+  );
+}
+
+class _playStoreState extends State<playStore> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                ),
+                onPressed: null
+            ),
+            Spacer(),
+            IconButton(
+                icon: Icon(
+                  Icons.bookmark,
+                  color: Colors.black,
+                ),
+                onPressed: null
+            ),
+            IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+                onPressed: null
+            ),
+            IconButton(
+                icon: Icon(
+                  Icons.more_vert,
+                  color: Colors.black,
+                ),
+                onPressed: null
+            )
+          ],
+        ),
+        body: ListView(children: [
+          _rowOne(),
+          _rowTwo(),
+          _rowThree(),
+          _rowFour(),
+          SizedBox(height:10.0),
+          _rowFive(),
+        ],),
+      ),
+    );
+  }
+}
